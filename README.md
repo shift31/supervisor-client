cea6298e928816d6291b446f224288bd801a4244
bgetsug <bgetsug@ibsys.com>
2013-06-04 23:25:49 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A Supervisor API client library for PHP
+
+Reference:
+* http://supervisord.org/api.html
---
+++ src/Shift31/SupervisorClient.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Shift31;
+
+use Zend\XmlRpc\Client;
+
+class SupervisorClient
+{
+	protected $_service;
+	protected $_logger;
+
+
+	/**
+	 * @param string		$supervisorUsername
+	 * @param string		$supervisorPassword
+	 * @param null|mixed	$logger
+	 */
+	public function __construct($supervisorUsername, $supervisorPassword, $logger = null)
+	{
+		$this->_logger = $logger;
+
+		// Connect to Supervsior XMLRPC server
+		// MUST add '/RPC2' to URI!!!
+		$xmlRpcClient = new Client("http://$supervisorUsername:$supervisorPassword@127.0.0.1:9001/RPC2");
+		$xmlRpcClient->setSkipSystemLookup(true);
+
+		$this->_service = $xmlRpcClient->getProxy();
+
+	}
+
+
+	public function getState()
+	{
+		$supervisorState = null;
+
+		try {
+			$state = $this->_service->supervisor->getState();
+			$supervisorState = $state['statename'];
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		return $supervisorState;
+	}
+
+
+	public function getAllProcessInfo()
+	{
+		$allProcessInfo = null;
+
+		try {
+			$allProcessInfo = $this->_service->supervisor->getAllProcessInfo();
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		return $allProcessInfo;
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function getAllProcessesByPid()
+	{
+		$processes = array();
+
+		try {
+			$allProcessInfo = $this->getAllProcessInfo();
+
+			foreach ($allProcessInfo as $process) {
+				$processes[$process['pid']] = $process;
+			}
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		return $processes;
+	}
+
+
+	/**
+	 * @param $name
+	 *
+	 * @return null|string
+	 */
+	public function getProcessInfo($name)
+	{
+		$processInfo = null;
+
+		try {
+			$processInfo = $this->_service->supervisor->getProcessInfo($name);
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		return $processInfo;
+	}
+
+
+	/**
+	 * @param $name
+	 *
+	 * @return null|string
+	 */
+	public function getProcessState($name)
+	{
+		$processState = null;
+
+		try {
+			$processInfo = $this->_service->supervisor->getProcessInfo($name);
+			$processState = $processInfo['statename'];
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		return $processState;
+	}
+
+
+	/**
+	 * @param      $name
+	 * @param bool $wait
+	 *
+	 * @return bool
+	 */
+	public function startProcess($name, $wait = true)
+	{
+
+		$result = false;
+
+		try {
+			$result = (bool)$this->_service->supervisor->startProcess($name, $wait);
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		if ($result === true) {
+			$this->_log('notice', "Started process '$name'");
+		} else {
+			$this->_log('err', "Failed to start process '$name'");
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * @param $name
+	 *
+	 * @return bool
+	 */
+	public function stopProcess($name)
+	{
+
+		$result = false;
+
+		try {
+			$result = (bool)$this->_service->supervisor->stopProcess($name);
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		if ($result === true) {
+			$this->_log('notice', "Stopped process '$name'");
+		} else {
+			$this->_log('err', "Failed to stop process '$name'");
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * @param string	$name
+	 * @param bool		$wait
+	 *
+	 * @return bool
+	 */
+	public function startProcessGroup($name, $wait = true)
+	{
+
+		$result = false;
+
+		try {
+			$result = (bool)$this->_service->supervisor->startProcessGroup($name, $wait);
+		} catch (\Exception $e) {
+			$this->_log('crit', "Exception: " . $e->getCode() . " - " . $e->getMessage());
+			$this->_log('debug', "Exception Stack Trace: " . $e->getTraceAsString());
+		}
+
+		if ($result === true) {
+			$this->_log('notice', "Started process group '$name'");
+		} else {
+			$this->_log('err', "Failed to start process '$name'");
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * @param string $priority
+	 * @param string $message
+	 */
+	protected function _log($priority, $message)
+	{
+		if ($this->_logger != null) {
+			$class = str_replace(__NAMESPACE__ . "\\", '', get_called_class());
+			$this->_logger->$priority("[$class] - $message");
+		}
+	}
+}
